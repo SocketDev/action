7d1d3f76df6ad5aabcf5bd38d6fdf74146510684
Carl Bergenhem <cbergenhem@gmail.com>
2026-07-08 11:27:52 -0400

    Add win32-arm64 to distributions

--- src/tools/firewall.js
+++ src/tools/firewall.js
@@ -13,7 +13,8 @@ const distributions = {
   'linux-arm64': 'linux-arm64',
   'darwin-x64': 'macos-x86_64',
   'darwin-arm64': 'macos-arm64',
-  'win32-x64': 'windows-x86_64.exe'
+  'win32-x64': 'windows-x86_64.exe',
+  'win32-arm64': 'windows-arm64.exe'
 }
 
 // executable name
