b9d37e9dc48c21116b0c49147094270376ef825e
jdalton <john.david.dalton@gmail.com>
2026-07-24 17:59:58 -0400

    chore: disable minification of dist bundles
    - Removes the terser minification plugin from the rollup config
    - The dist bundles are now readable and easier to review in PRs

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,8 @@
 import commonjs from '@rollup/plugin-commonjs'
-import terser from '@rollup/plugin-terser'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 
 const config = {
-  plugins: [commonjs(), nodeResolve({ preferBuiltins: true }), terser()]
+  plugins: [commonjs(), nodeResolve({ preferBuiltins: true })]
 }
 
 export default config
